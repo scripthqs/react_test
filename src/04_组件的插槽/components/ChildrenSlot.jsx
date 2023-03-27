94042e54bf3015df8bd2e49ad74aeed2971f6767
scripthqs <1941186772@qq.com>
2023-03-27 08:50:58 +0800

    1

--- src/04_组件的插槽/components/ChildrenSlot.jsx
+++ src/04_组件的插槽/components/ChildrenSlot.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 export class ChildrenSlot extends Component {
   render() {
     const { children } = this.props;
