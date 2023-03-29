4717c1560d6085927395dd47c73dd4f75f743708
scripthqs <1941186772@qq.com>
2023-03-29 16:33:03 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React, { PureComponent } from "react";
-import Hello from "./components/Hello";
-import Counter from "./components/Counter";
-import HelloMsg from "./components/HelloMsg";
-
-import UseEffect from "./02_useEffect/UseEffect";
 
 export class App extends PureComponent {
   render() {
     return (
       <div>
         <h1>App</h1>
-        <UseEffect></UseEffect>
-        <hr />
-        <HelloMsg></HelloMsg>
-        <hr />
-        <Counter></Counter>
-        <hr />
-        <Hello></Hello>
       </div>
     );
   }
